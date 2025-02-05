8f87252c12696bfb88d11c3c96a88bbb886af8d3
Golagani Rashmika <golagani.rashmika@accenture.com>
2025-02-05 13:23:31 +0530

    adding user input component searchbar which helps to filter the products

--- src/App.jsx
+++ src/App.jsx
@@ -45,3 +45,13 @@ function ProductTable({ products }) {
     </table>
   );
 }
+function SearchBar() {
+  return (
+    <form>
+      <input type="text" placeholder="Search..." />
+      <label>
+        <input type="checkbox" /> Only show products in stock
+      </label>
+    </form>
+  );
+}
