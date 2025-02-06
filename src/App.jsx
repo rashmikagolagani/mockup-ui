91333bd623d5a0f8c3a949783f3925550f295350
Golagani Rashmika <golagani.rashmika@accenture.com>
2025-02-06 16:43:20 +0530

    gave the products list and created an App component where the products are used by FilterablePraductTable

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ function ProductCategoryRow({ category }) {
   );
 }
 function ProductRow({ product }) {
-  const name = ProductCategoryRow.stocked ? (
-    ProductCategoryRow.name
+  const name = product.stocked ? (
+    product.name
   ) : (
-    <span style={{ color: "red" }}>{ProductCategoryRow.name}</span>
+    <span style={{ color: "red" }}>{product.name}</span>
   );
   return (
     <tr>
@@ -18,7 +18,7 @@ function ProductRow({ product }) {
     </tr>
   );
 }
-export default function ProductTable({ products }) {
+function ProductTable({ products }) {
   const rows = [];
   let lastCategory = null;
   products.forEach((product) => {
@@ -64,4 +64,16 @@ function FilterableProductTable({ products }) {
       <ProductTable products={products} />
     </div>
   );
+}
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
+
+export default function App() {
+  return <FilterableProductTable products={PRODUCTS} />;
 }
